2cf496c476f80abe84bb79f360d264158d273a18
Daniel Persson <persson.daniel.1990@gmail.com>
2021-10-19 10:08:22 +0200

    Add model validation

---
+++ Validation/ModelValidation.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using dotnet_api_test.Exceptions.ExceptionResponses;
+using dotnet_api_test.Models.Dtos;
+
+namespace dotnet_api_test.Validation
+{
+    public static class ModelValidation
+    {
+        public static void ValidateCreateDishDto(CreateDishDto entity)
+        {
+            var validationResults = new List<ValidationResult>();
+            ValidateEntityPropertyIsNotNull(nameof(entity.Name),entity.Name, validationResults);
+            ValidateEntityPropertyIsNotNull(nameof(entity.Cost), entity.Cost, validationResults);
+            ValidateEntityPropertyIsNotNull(nameof(entity.MadeBy), entity.MadeBy, validationResults);
+
+            if (validationResults.Count > 0)
+            {
+                throw new BadRequestExceptionResponse("Model is not valid because following properties are missing: " + string.Join(", ", validationResults.Select( s => s.ErrorMessage).ToArray()));
+            }
+        }
+        
+        public static void ValidateUpdateDishDto(UpdateDishDto entity)
+        {
+            var validationResults = new List<ValidationResult>();
+            ValidateEntityPropertyIsNotNull(nameof(entity.Name),entity.Name, validationResults);
+            ValidateEntityPropertyIsNotNull(nameof(entity.Cost), entity.Cost, validationResults);
+            ValidateEntityPropertyIsNotNull(nameof(entity.MadeBy), entity.MadeBy, validationResults);
+
+            if (validationResults.Count > 0)
+            {
+                throw new BadRequestExceptionResponse("Model is not valid because following properties are missing: " + string.Join(", ", validationResults.Select( s => s.ErrorMessage).ToArray()));
+            }
+        }
+
+        private static void ValidateEntityPropertyIsNotNull<T>(string entityName, T value, ICollection<ValidationResult> validationResults)
+        {
+            if (value == null)
+            {
+                validationResults.Add(new ValidationResult(entityName));
+            }
+        }
+    }
+}
